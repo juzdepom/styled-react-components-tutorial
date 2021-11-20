b486b78f8eba07deea0c1d71d5bf4fc3733a851b
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-11-20 21:45:08 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
+import { Container } from './components/styles/Container.styled';
+import GlobalStyles from './components/styles/Global';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <ThemeProvider theme = {theme}>
+      <>
+      <GlobalStyles/>
+        <Header/>
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
